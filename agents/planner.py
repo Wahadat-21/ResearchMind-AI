4192a33013146f1580e90183abc13e0615b93ef1
Syed Sian Wahadat <sianwahadat23@gmail.com>
2026-06-11 00:02:32 +0600

    Create planner.py

---
+++ agents/planner.py
@@ -0,0 +1,10 @@
+class PlannerAgent:
+
+    def create_plan(self, topic):
+
+        return [
+            f"Research background of {topic}",
+            f"Find latest developments about {topic}",
+            f"Collect statistics for {topic}",
+            f"Generate report for {topic}"
+        ]
